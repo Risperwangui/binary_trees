7e16cce2176fb8dd238662153300908f9bb379f6
Risperwangui <resperwangui68@gmail.com>
2023-08-03 07:08:22 -0700

    fixing bigs

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 /**
  * binary_trees_ancestor - this finds the lowest common ancestor
- * @first: pointer 
+ * @first: pointer
  * @second: pointer
  * Return: a pointer to the lowest common ancestor node
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	const binary_tree_t *dad, *mom;
 
